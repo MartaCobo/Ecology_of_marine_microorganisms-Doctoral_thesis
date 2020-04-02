568df7c6ea92da36536f5cb6d1e18d4da7543e20
MartaCobo <60861771+MartaCobo@users.noreply.github.com>
2020-04-02 19:26:47 +0200

    Update CopyNumber_MADs_Calculation.r

--- CopyNumber_MADs_Calculation.r
+++ CopyNumber_MADs_Calculation.r
@@ -2,17 +2,30 @@
 
 #                            (c) Marta Cobo-Simón, CNB-CSIC.
 
-# This script calculates the average copy number per genome and highly-variable genes from a table of TPMs whose columns are samples and the rows are KOs or COGs. 
+# This script calculates the average copy number per genome and highly-variable genes. 
+# It requires a table of TPMs whose columns are samples and the rows are KOs or COGs. 
+
 
 # Usage: source("CopyNumber_MADs_Calculation.R"). Choose the Universal Single Copy Genes (singleCopy) in KO or COG. 
+
 # In the folder where the script is run, there must be the table with TPMs. The following outputs will be saved in the same folder:
+
 # CopyNumber_longformat.txt -> Average copy number per genome of different functions at different samples in long format.
+
 # CopyNumber_wideformat.txt -> Average copy number per genome of different functions at different samples in wide format.
-# Highly-Variable.txt -> Highly-Variable genes defined as the ones whose Median Absolute Deviation (MAD) is higher than twice the standard deviation.
-# Highly-Variable.png -> Variation in copy number of Universal Single-Copy Genes (USiCGs) represented in boxplots for each sample in comparison to highly-variable genes. 
-# The variable kruskalwalliscopynumberusicgs contains the Kruskal-Wallis test for USiCGs. It must be non-significant. 
+
+# Highly-Variable.txt -> Highly-Variable genes defined as the ones whose Median Absolute Deviation (MAD) is higher than twice the 
+# standard deviation.
+
+# Highly-Variable.png -> Variation in copy number of Universal Single-Copy Genes (USiCGs) represented in boxplots for each sample in 
+# comparison to highly-variable genes. 
+
+# The variable kruskalwalliscopynumberusicgs contains the Kruskal-Wallis test for USiCGs. It must be non-significant.
+
 # The variable kruskalwalliscopynumbertodos contains the Kruskal-Wallis test for all the genes. 
+
 # The variable foldchangeusicgsintersample contains the log2(foldchange) of USiCGs among different samples.
+
 # The variable foldchangeusicgsintrasample contains the log2(foldchange) of USiCGs in each sample. 
 
 
@@ -21,18 +34,21 @@ peleae10 = read.table("tablasjuntasordenado.txt",header=T,sep="\t",row.names=1)
 
 
 # Select which group of USiCGs must be used.
-singleCopy = c('K02863','K02867','K02871','K02874','K02876','K02886','K02931','K02933','K02946','K02948','K02950','K02952','K02986','K02992','K02994')  # We used a set of 35 OGs that are widely conserved (present in most species), rarely occur as duplicate genes in known genomes, are not subject to horizontal gene transfer, and do not scale with genome size as marker genes -> estos genes están en este grupo de 35 usados en Raes et al., 2007.
-#singleCopy = c('COG0081','COG0080','COG0102','COG0093','COG0200','COG0090','COG0094','COG0097','COG0051','COG0100','COG0048','COG0099','COG0522','COG0049','COG0096')  
+
+# Set of 15 OGs that are widely conserved (present in most species), rarely occur as duplicate genes in known genomes, are not subject 
+# to horizontal gene transfer, and do not scale with genome size as marker genes (Raes et al., 2007).
+singleCopy = c('K02863','K02867','K02871','K02874','K02876','K02886','K02931','K02933','K02946','K02948','K02950','K02952','K02986',
+	       'K02992','K02994')  
+#singleCopy = c('COG0081','COG0080','COG0102','COG0093','COG0200','COG0090','COG0094','COG0097','COG0051','COG0100','COG0048','COG0099',
+#               'COG0522','COG0049','COG0096')  
 
 usicgspelae10reducidotabla <- subset(peleae10, rownames(peleae10) %in% singleCopy) # Extract only rows with USiCGs.
 medianasusicgs <- colMeans(usicgspelae10reducidotabla) # Median of TPM USiCGs in each sample.  
 copynumber <- t(t(peleae10) / medianasusicgs) # Calculation of Copy Number. 
-
 write.table(copynumber, file="CopyNumber_wideformat.txt", sep="\t", quote=F) # Table in wide format with the copy number. 
 
 
 # Calculation of MADs and selection of highly-variable genes. 
-
 
 library(matrixStats)
 MADs <- as.data.frame(rowMedians(as.matrix(abs(copynumber - rowMedians(as.matrix(copynumber))))), row.names=rownames(copynumber))
@@ -40,17 +56,15 @@ hipervariablesMADs <- subset(copynumber, subset=MADs > (2 * colSds(as.matrix(MAD
 MADstablatodos <- as.data.frame(as.matrix(abs(copynumber - rowMedians(as.matrix(copynumber))), row.names=rownames(copynumber)))
 
 
-
 # Table format from wide format to long format. 
 library(reshape2)
 listacopynumber <- melt(copynumber)
-
 colnames(listacopynumber) <- c("cog", "muestra", "rpkm") # Change the name of the columns.
 listausicgspelae10reducido <- subset(listacopynumber, listacopynumber$cog %in% singleCopy) # Extract only rows with USiCGs. 
 kruskalwalliscopynumberusicgs <- oneway.test(rpkm ~ muestra, data = listausicgspelae10reducido)
 kruskalwalliscopynumbertodos <- oneway.test(rpkm ~ muestra, data = listacopynumber)
-
 write.table(listacopynumber, file="listacopynumber.txt", sep="\t", quote=F)
+
 
 # Highly-variable genes.
 
