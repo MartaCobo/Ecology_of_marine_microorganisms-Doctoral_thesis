36f16187b4c2bea4f6fa095dceec4ce8715ddeee
MartaCobo <60861771+MartaCobo@users.noreply.github.com>
2020-04-02 19:30:45 +0200

    Update TPM_Calculation.pl

--- TPM_Calculation.pl
+++ TPM_Calculation.pl
@@ -1,4 +1,3 @@
-
 #!/usr/bin/perl
 use strict;
 use warnings;
@@ -18,22 +17,28 @@ my $USAGE=<<"USAGE" ;
 
 
 
-Part of the doctoral thesis "Ecology of marine microorganisms: biodiversity, genomics and metagenomics". 02/04/2020 Original version,
+ Part of the doctoral thesis "Ecology of marine microorganisms: biodiversity, genomics and metagenomics". 02/04/2020 Original version,
 
                             (c) Marta Cobo-Simón, CNB-CSIC.
-
-This script calculates the TPM (Transcripts Per Million of reads) of KOs from metagenomes per sample and per taxon. The reason for using TPM instead of RPKM is that the sum of all TPMs in each sample are the same. This makes it easier to compare the proportion of reads that mapped to a gene in each sample. In contrast, with RPKM and FPKM, the sum of the normalized reads in each sample may be different, and this makes it harder to compare samples directly.
+ 
+ This script calculates the TPM (Transcripts Per Million of reads) of KOs from metagenomes per sample and per taxon. The reason for 
+ using TPM instead of RPKM is that the sum of all TPMs in each sample are the same. This makes it easier to compare the proportion of 
+ reads that mapped to a gene in each sample. In contrast, with RPKM and FPKM, the sum of the normalized reads in each sample may be 
+ different, and this makes it harder to compare samples directly.
  
  It requires: 
 	- The following output files from SqueezeMeta (Tamames & Puente-Sánchez, 2019):
 		- The file .orftable
 		- The file .contiglog
-	- A file with the selected samples to analyze, one sample in each row. Recommendation: use the samples where the selected taxa is abundant, for example, higher than 0.7% of the sample. Example:
+	- A file with the selected samples to analyze, one sample in each row. 
+	Recommendation: use the samples where the selected taxa is abundant, for example, higher than 0.7% of the sample. 
+	Example:
 		MP0311
 		MP0313
 		MP0321
 		
-	The result is a file called TPM_<selected_taxon>.txt, consisting on a table in long format saved in the output path selected, which contains the KO, the sample and the TPM. 
+	The result is a file called TPM_<selected_taxon>.txt, consisting on a table in long format saved in the output path selected, 
+	which contains the KO, the sample and the TPM. 
 		
 		KO	Sample	TPM
 		K02010	MP0311	3000
@@ -55,7 +60,8 @@ This script calculates the TPM (Transcripts Per Million of reads) of KOs from me
  
  Example:
  
-	perl $PROGNAME Pelagibacterales SamplesPelagibacterales.txt 12.coassembly.orftable 08.coassembly.contiglog /home/mcobo province10.samples kegg
+	perl $PROGNAME Pelagibacterales SamplesPelagibacterales.txt 12.coassembly.orftable 08.coassembly.contiglog /home/mcobo 
+	province10.samples kegg
 
 
 
@@ -93,7 +99,8 @@ while(<$infile>) {
 close $infile;
 
 
-# Selection from .contiglog of those contigs assigned to the selected taxon. They will be used to recover genes whose contig is assigned to the chosen taxon although the gene was not assigned. 
+# Selection from .contiglog of those contigs assigned to the selected taxon. They will be used to recover genes whose contig is assigned
+# to the chosen taxon although the gene was not assigned. 
 
 my %contigs;
 open (my $infile2, "$contigstable") || die "I cannot open the .contiglog file\n";
@@ -109,7 +116,8 @@ while(<$infile2>) {
 
 close $infile2;
 
-# From the .orftable, selection of the raw counts (number of reads mapped against one particular gene predicted from the assembly of the metagenome(s)). Select the genes assigned to the selected taxon or the genes whose contig is assigned to the selected taxon. 
+# From the .orftable, selection of the raw counts (number of reads mapped against one particular gene predicted from the assembly of the
+# metagenome(s)). Select the genes assigned to the selected taxon or the genes whose contig is assigned to the selected taxon. 
 
 my $numerofilas = 0;
 open (my $infile3, "$ficheromuestras") || die "I cannot open the .samples file\n";
@@ -160,16 +168,32 @@ for ( my $i = -$numeromuestras; $i <= -1; $i++ ) {
 						my $orf = $lista2[0];
 						my $contig = $lista2[1];
 						my $rawcounts = $lista2[$i]; 
-						if (( $keggstring =~ /\*/) && ( $rawcounts ne "") && ($rawcounts ne "0") && ($rawcounts ne "0.000") && ( @keggs) && ( $sample ne "" ) && ( $lengthorf ne "" ) && ( $taxonomiacompleta ne "" ) && ( $orf ne "")) { # best average
+						
+						# Best average.
+						if (( $keggstring =~ /\*/) && ( $rawcounts ne "") && ($rawcounts ne "0") && 
+						($rawcounts ne "0.000") && ( @keggs) && ( $sample ne "" ) && ( $lengthorf ne "" ) && 
+						( $taxonomiacompleta ne "" ) && ( $orf ne "")) {
+						
 							my $numero = scalar (@keggs);
-							my $newrawcounts = $rawcounts / scalar (@keggs); # Since one gene can be assigned to two or more KOs, the raw reads are divided by the number of KOs. 
+							
+							# Since one gene can be assigned to two or more KOs, the raw reads are divided by
+							# the number of KOs. 
+							my $newrawcounts = $rawcounts / scalar (@keggs); 
 							if (( $taxonomiacompleta =~ /$generoescogido/) or ( defined $contigs{$contig})) {
 								my $genero = "$generoescogido";
 								foreach my $kegg ( @keggs ) {
-									$kegg =~ s/\*//; # Only KOs, COGs or PFAMs assigned with best average algorithm (SqueezeMeta), marked by '*', are recovered. The '*' is removed. 
-									$SampleKEGGTaxaGeneRawcounts{$sample}{$genero}{$kegg}{$orf} = $newrawcounts;
-									my $lengthorfgen = 3 * $lengthorf; # Calculation of gene length from protein length.
-									$sample_kegg_genero_orf_length{$sample}{$kegg}{$genero}{$orf} = $lengthorfgen;
+								
+									# Only KOs, COGs or PFAMs assigned with best average algorithm 
+									# (SqueezeMeta), marked by '*', are recovered. The '*' is removed. 
+									$kegg =~ s/\*//; 
+									
+									$SampleKEGGTaxaGeneRawcounts{$sample}{$genero}{$kegg}{$orf} = 
+									$newrawcounts;
+									
+									# Calculation of gene length from protein length.
+									my $lengthorfgen = 3 * $lengthorf;
+									$sample_kegg_genero_orf_length{$sample}{$kegg}{$genero}{$orf} = 
+									$lengthorfgen;
 								}
 							}
 						}
@@ -183,8 +207,8 @@ for ( my $i = -$numeromuestras; $i <= -1; $i++ ) {
 
 my %SampleGeneroKEGGRawcounts;
 
-# Calculation of the abundance of each function (KO, COG or PFAM) in each sample for the selected taxon. 
 
+# Calculation of the abundance of each function (KO, COG or PFAM) in each sample for the selected taxon. 
 
 foreach my $sample ( sort keys %SampleKEGGTaxaGeneRawcounts ) {
 	foreach my $genero ( sort keys %{$SampleKEGGTaxaGeneRawcounts{$sample}} ) {
@@ -205,15 +229,15 @@ my %sample_kegg_genero_medianalongitudes;
 
 foreach my $sample ( sort keys %sample_kegg_genero_orf_length ) {
 	foreach my $kegg ( sort keys %{$sample_kegg_genero_orf_length{$sample}} ) {
-        foreach my $genero ( sort keys %{$sample_kegg_genero_orf_length{$sample}{$kegg}} ) {
-            my @longitudes = ();
-            foreach my $orf ( sort keys %{$sample_kegg_genero_orf_length{$sample}{$kegg}{$genero}} ) {
-                push @longitudes, $sample_kegg_genero_orf_length{$sample}{$kegg}{$genero}{$orf};
-            }
-            my $medianalongitudes = median (@longitudes);
-            $sample_kegg_genero_medianalongitudes{$sample}{$kegg}{$genero} = $medianalongitudes;
+        	foreach my $genero ( sort keys %{$sample_kegg_genero_orf_length{$sample}{$kegg}} ) {
+            		my @longitudes = ();
+            		foreach my $orf ( sort keys %{$sample_kegg_genero_orf_length{$sample}{$kegg}{$genero}} ) {
+               		 	push @longitudes, $sample_kegg_genero_orf_length{$sample}{$kegg}{$genero}{$orf};
+            		}
+            		my $medianalongitudes = median (@longitudes);
+            		$sample_kegg_genero_medianalongitudes{$sample}{$kegg}{$genero} = $medianalongitudes;
 		}
-    }
+    	}
 }
 
 
@@ -223,16 +247,22 @@ my %sample_genero_kegg_tpm;
 my %sample_genero_kegg_abundancianormalizadalongitud;
 my %sample_genero_total;
 
+
 # Abundancies normalized by KO, COG or PFAM length.
 
 foreach my $sample ( sort keys %SampleGeneroKEGGRawcounts ) {
 	if ( defined $muestras{$sample} ) {
-        foreach my $genero ( sort keys %{$SampleGeneroKEGGRawcounts{$sample}} ) {
-            foreach my $kegg ( sort keys %{$SampleGeneroKEGGRawcounts{$sample}{$genero}} ) {
-                my $abundancianormalizadalongitud = $SampleGeneroKEGGRawcounts{$sample}{$genero}{$kegg} * 1000 / $sample_kegg_genero_medianalongitudes{$sample}{$kegg}{$genero}; # longitud kegg por muestra
-				$sample_genero_kegg_abundancianormalizadalongitud{$sample}{$genero}{$kegg} = $abundancianormalizadalongitud;
-            }
-        }
+       		foreach my $genero ( sort keys %{$SampleGeneroKEGGRawcounts{$sample}} ) {
+            		foreach my $kegg ( sort keys %{$SampleGeneroKEGGRawcounts{$sample}{$genero}} ) {
+	    
+	    			# Longitud kegg por muestra.
+               			my $abundancianormalizadalongitud = $SampleGeneroKEGGRawcounts{$sample}{$genero}{$kegg} * 1000 / 
+				$sample_kegg_genero_medianalongitudes{$sample}{$kegg}{$genero}; 
+		
+				$sample_genero_kegg_abundancianormalizadalongitud{$sample}{$genero}{$kegg} = 
+				$abundancianormalizadalongitud;
+           		}
+       		}
 	}
 }
 
@@ -254,7 +284,9 @@ foreach my $sample ( sort keys %sample_genero_kegg_abundancianormalizadalongitud
 foreach my $sample ( sort keys %sample_genero_kegg_abundancianormalizadalongitud ) {
         foreach my $genero ( sort keys %{$sample_genero_kegg_abundancianormalizadalongitud{$sample}} ) {
 		foreach my $kegg (  sort keys %{$sample_genero_kegg_abundancianormalizadalongitud{$sample}{$genero}} ) {
-			my $tpm = $sample_genero_kegg_abundancianormalizadalongitud{$sample}{$genero}{$kegg} * 1000000 / $sample_genero_total{$sample}{$genero};
+			my $tpm = $sample_genero_kegg_abundancianormalizadalongitud{$sample}{$genero}{$kegg} * 1000000 / 
+			$sample_genero_total{$sample}{$genero};
+			
 			$sample_genero_kegg_tpm{$genero}{$kegg}{$sample} = $tpm;
 		}
 	}
@@ -264,7 +296,10 @@ foreach my $sample ( sort keys %sample_genero_kegg_abundancianormalizadalongitud
 
 foreach my $genero ( sort keys %sample_genero_kegg_tpm) {
 	open( my $outfile, ">$outputpath/TPM_$genero.txt") || die "No puedo abrir el fichero output2\n";
-	print $outfile "#--Created by $0, ", scalar localtime, "\n";  # Write in the file the script that produced it and the date of execution. 
+	
+	# Write in the file the script that produced it and the date of execution. 
+	print $outfile "#--Created by $0, ", scalar localtime, "\n";  
+	
 	print $outfile "KO\tSample\tTPM\n";
 	foreach my $kegg ( sort keys %{$sample_genero_kegg_tpm{$genero}} ) {
 		foreach my $sample ( sort keys %{$sample_genero_kegg_tpm{$genero}{$kegg}} ) {
@@ -273,6 +308,7 @@ foreach my $genero ( sort keys %sample_genero_kegg_tpm) {
 	}
 	close $outfile;
 }
+
 
 # Function to calculate the median.
 
